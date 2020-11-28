e26b72cb29b622fbdaf61fee3cc88bb4c084759e
Manish Bhattarai <bhaimanish00@gmail.com>
2020-11-28 22:33:25 +0545

    Soil Moisture Sensor + Water Pump
    Water is pumped to the farm if the moisture is 60% and below.

--- smartfarmer/smartfarmer.ino
+++ smartfarmer/smartfarmer.ino
@@ -1,28 +1,18 @@
-//int relayPin = 7;
-//
-//void setup() {
-//  pinMode(relayPin, OUTPUT);
-//
-//}
-//
-//void loop() {
-//  digitalWrite(relayPin, HIGH);
-//  delay(5000);
-//  digitalWrite(relayPin, LOW);
-//  delay(5000);    
-//}
-
 int sensor_pin = A0; //Soil Sensor input at Analog Pin A0
 
 int output_value ;
 
+int relayPin = 7;
+
 void setup() {
 
    Serial.begin(9600);
+   
+   pinMode(relayPin, OUTPUT);
 
    Serial.println("Reading From the Sensor ...");
 
-   delay(2000);
+//   delay(2000);
 
    }
 
@@ -31,18 +21,28 @@ void loop() {
    output_value= analogRead(sensor_pin);
 
    output_value = map(output_value,1019,303,0,100);//Mapping output value to percentage Test Data Dry : 1023 Wet:303
-   Serial.print("Moisture : ");
+   Serial.print("Soil Moisture : ");
    if(output_value > 100) {
 
    Serial.print("100");
+   Serial.print("Enough Water");
    }
 
+   else if(output_value <=60)
+   {
+    Serial.print(output_value);
+    Serial.println("%");
+    Serial.println("Lack of water, pumping water into the field");
+    digitalWrite(relayPin, LOW);
+   }
    else {
     Serial.print(output_value);
+    Serial.println("%");
+    Serial.println("Enough Water");
+    digitalWrite(relayPin, HIGH);
    }
 
-   Serial.println("%");
 
-   delay(1000);
+   delay(500);
 
    }
